804dbfaffa490ad3381cead04eb428f51a4387ff
PabloMateosP <pmatpal0105@g.educaand.es>
2022-11-23 10:36:07 +0100

    Calculadora

---
+++ src/Gato.java
@@ -0,0 +1,19 @@
+public class Gato {
+    private String nombre;
+    private static String raza;
+    Gato (String nombre){
+        this.nombre = nombre;
+    }
+    String getNombre() {
+        return nombre;
+    }
+    String getRaza() {
+        return raza;
+    }
+    void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    void setRaza(String raza){
+        this.raza = raza;
+    }
+}
---
+++ src/repaso.java
@@ -0,0 +1,25 @@
+public class repaso {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+        Saludar("Manolo");
+
+        //Creamos un gato
+        String s = "miCadena";
+        //Constructor por defecto es el mismo de nombre que la clase en este caso Gato
+        Gato misifu = new Gato("Misifú");
+        Gato isidoro = new Gato ("Isidoro");
+        Gato sonia = new Gato ("Sonia");
+        System.out.println("Mi gato se llama: " + misifu.getNombre());
+        System.out.println("Mi gato se llama: " + isidoro.getNombre());
+        System.out.println("Mi gato se llama: " + sonia.getNombre());
+
+        //Cambiamos nombre al gato
+        //Cuando ponemos el punto (operador de acceso) nos aparecen las funciones que se pueden hacer
+        misifu.setNombre("Pedro");
+        System.out.println("Mi gato Misifu ahora se llama: " + misifu.getNombre());
+
+    }
+    public static void Saludar (String nombre){
+        System.out.println("Hola " + nombre);
+    }
+}
